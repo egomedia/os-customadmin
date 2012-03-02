47b5fa41dc92ec2d97727bb36949dc812988e658
egomedia <oli@egomedia.co.uk>
2012-03-02 16:08:47 +0000

    launch as self contained object, made certain functions optional

--- os-customadmin.php
+++ os-customadmin.php
@@ -2,14 +2,16 @@
 /*
 Plugin Name: OS Custom Admin
 Description: Cleans up the Wordpress admin to make more user-friendly for corporate clients.
-Version: 0.8
+Version: 1.0
 Author: Oli Salisbury
 */
-
+/*
+//you need to initialise the class in your functions.php file
 if (WP_ADMIN) {
-	$os_customadmin = new OS_CustomAdmin();
-	//$os_customadmin->rename_post_object('Car', 'Cars');
+	include_once('scripts/os-customadmin.php');
+	new OS_CustomAdmin();
 }
+*/
 
 class OS_CustomAdmin {
 	
@@ -19,21 +21,21 @@ class OS_CustomAdmin {
 		add_action('right_now_content_table_end' , array($this, 'add_all_post_types_to_dashboard'));
 		add_action('wp_dashboard_setup', array($this, 'remove_dashboard_widgets'));
 		add_action('admin_head', array($this, 'remove_dashboard_discussion'));
-		add_action('admin_menu', array($this, 'remove_menu_items'));
-		add_action('admin_menu', array($this, 'remove_submenus'));
-		add_action('admin_init', array($this, 'customize_meta_boxes'));
+		add_action('admin_menu', array($this, 'remove_surpless_menus'));
+		add_action('admin_menu', array($this, 'remove_surpless_submenus'));
 		add_filter('manage_posts_columns', array($this, 'custom_post_columns'));
 		add_filter('manage_pages_columns', array($this, 'custom_pages_columns'));
 		add_filter('manage_media_columns', array($this, 'custom_media_columns'));
 		add_action('admin_bar_menu', array($this, 'custom_admin_bar'), 1000);
-		add_action('admin_head', array($this, 'hide_add_new_page_button'));
 		add_action('admin_menu', array($this, 'hide_updates_nag'));
 		add_filter('tiny_mce_before_init', array($this, 'custom_tiny_mce'));
+		add_action('admin_init', array($this, 'custom_meta_boxes'));
 		$this->rename_post_object();
 	}
 	
+	//TOP LEVEL FUNCTIONS
+	
 	//limit upload size to 350kb for images
-	//v1.1
 	function reduce_max_upload_limit($file) {
 		$size = $file['size'];
 		$allowed_exts = array('pdf', 'doc');
@@ -63,7 +65,7 @@ class OS_CustomAdmin {
 		}
 	}
 	
-	//customise admin screens
+	//remove dashboard widgets
 	function remove_dashboard_widgets(){
 		global$wp_meta_boxes;
 		unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_plugins']);
@@ -75,15 +77,22 @@ class OS_CustomAdmin {
 		unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_recent_drafts']);
 	}
 	
-	function remove_dashboard_discussion() { //, .b-posts, .posts
+	//remove dashboard discussion, cats, and tags
+	function remove_dashboard_discussion() {
 		echo '
 		<style type="text/css">
-		#dashboard_right_now .table_discussion, .b-cats, .cats, .b-tags, .tags { display:none !important; }
-		#tab-type_url { display:none !important; }
+		#dashboard_right_now .table_discussion, 
+		#dashboard_right_now .b-cats, 
+		#dashboard_right_now .cats, 
+		#dashboard_right_now .b-tags, 
+		#dashboard_right_now .tags,
+		#tab-type_url 
+		{ display:none !important; }
 		</style>';
 	}
 	
-	function remove_menu_items() { //, __('Posts')
+	//remove surpless menu items
+	function remove_surpless_menus() { 
 		global $menu;
 		$restricted = array(__('Links'), __('Comments'), __('Profile'), __('Tools'));
 		end($menu);
@@ -93,15 +102,73 @@ class OS_CustomAdmin {
 		}
 	}
 	
-	function remove_submenus() {
+	//remove surpless submenus
+	function remove_surpless_submenus() {
 		global $submenu;
 		unset($submenu['index.php'][10]); //removes updates
 		unset($submenu['edit.php'][15]); //removes categories
 		unset($submenu['edit.php'][16]); //removes tags
-		unset($submenu['edit.php?post_type=page'][10]); //removes add new page
 	}
 	
-	function customize_meta_boxes() {
+	//tidy up admin archive pages
+	function custom_post_columns($defaults) {
+		unset($defaults['comments']);
+		unset($defaults['author']);
+		unset($defaults['categories']);
+		unset($defaults['tags']);
+		return $defaults;
+	}
+	function custom_pages_columns($defaults) {
+		unset($defaults['comments']);
+		unset($defaults['author']);
+		unset($defaults['date']);
+		return $defaults;
+	}
+	function custom_media_columns($defaults) {
+		unset($defaults['comments']);
+		unset($defaults['author']);
+		return $defaults;
+	}
+	
+	//customise admin bar
+	function custom_admin_bar($wp_admin_bar) { 
+		//remove wordpress tab
+		$wp_admin_bar->remove_node('wp-logo'); 
+		//remove visit site tab
+		$wp_admin_bar->remove_node('view-site');
+		//remove updates tab
+		$wp_admin_bar->remove_node('updates');
+		//remove comments tab
+		$wp_admin_bar->remove_node('comments');
+		//remove add new subtabs
+		$wp_admin_bar->remove_node('new-media');
+		$wp_admin_bar->remove_node('new-link');
+		$wp_admin_bar->remove_node('new-user');
+		//update myaccount tab
+		$myaccount = $wp_admin_bar->get_node('my-account');
+		$wp_admin_bar->add_node(array('id'=>'my-account', 'title'=>str_replace("Howdy", "Welcome", $myaccount->title)));
+	}
+	
+	//hide updates nag from non admins
+	function hide_updates_nag() {
+		if (!current_user_can('administrator')) {
+			remove_action('admin_notices', 'update_nag', 3);
+		}
+	}
+	
+	//custom tinymce editor
+	function custom_tiny_mce($in) {
+		if (!current_user_can('administrator')) {
+			$in['theme_advanced_buttons1']='formatselect,|,bold,italic,|,link,unlink,|,bullist,numlist,|,undo,redo,|,wp_more';//,|,wp_adv';
+			$in['theme_advanced_buttons2']='justifyleft,justifycenter,justifyright,|,forecolor,|,removeformat,|,charmap,|,wp_fullscreen';
+			$in['theme_advanced_buttons3']='';
+			$in['theme_advanced_buttons4']='';
+		}
+		return $in;
+	}
+	
+	//customise default meta boxes
+	function custom_meta_boxes() {
 		//removes meta boxes from posts
 		remove_meta_box('postcustom','post','normal');
 		remove_meta_box('trackbacksdiv','post','normal');
@@ -124,28 +191,7 @@ class OS_CustomAdmin {
 		//remove_meta_box('slugdiv','page','normal');
 	}
 	
-	function custom_post_columns($defaults) {
-		unset($defaults['comments']);
-		unset($defaults['author']);
-		unset($defaults['categories']);
-		unset($defaults['tags']);
-		return $defaults;
-	}
-	
-	function custom_pages_columns($defaults) {
-		unset($defaults['comments']);
-		unset($defaults['author']);
-		unset($defaults['date']);
-		return $defaults;
-	}
-	
-	function custom_media_columns($defaults) {
-		unset($defaults['comments']);
-		unset($defaults['author']);
-		return $defaults;
-	}
-	
-	//rename_post_object
+	//RENAME POSTS
 	function rename_post_object($label_single='News', $label_plural='News') {
 		$this->label_single = $label_single;
 		$this->label_plural = $label_plural;
@@ -182,27 +228,56 @@ class OS_CustomAdmin {
 		$wp_admin_bar->add_node(array('id'=>'new-post', 'title'=>$this->label_single));
 	}
 	
-	//custom admin bar
-	function custom_admin_bar($wp_admin_bar) { 
-		//remove wordpress tab
-		$wp_admin_bar->remove_node('wp-logo'); 
-		//remove visit site tab
-		$wp_admin_bar->remove_node('view-site');
-		//remove updates tab
-		$wp_admin_bar->remove_node('updates');
-		//remove comments tab
-		$wp_admin_bar->remove_node('comments');
-		//remove add new subtabs
-		$wp_admin_bar->remove_node('new-page');
-		$wp_admin_bar->remove_node('new-media');
-		$wp_admin_bar->remove_node('new-link');
-		$wp_admin_bar->remove_node('new-user');
-		//update myaccount tab
-		$myaccount = $wp_admin_bar->get_node('my-account');
-		$wp_admin_bar->add_node(array('id'=>'my-account', 'title'=>str_replace("Howdy", "Welcome", $myaccount->title)));
+	
+	//DISABLE POSTS
+	function disable_posts() {
+		add_action('admin_menu', array($this, 'remove_posts_menu'));
+		add_action('admin_bar_menu', array($this, 'remove_posts_admin_bar'), 1000);
+		add_action('admin_head', array($this, 'remove_posts_dashboard'));
 	}
 	
-	//hide 'add new' page button
+	function remove_posts_menu() { 
+		global $menu;
+		$restricted = array('edit_posts');
+		end($menu);
+		while (prev($menu)){
+			$value = explode(' ',$menu[key($menu)][1]);
+			if(in_array($value[0] != NULL?$value[0]:"" , $restricted)){unset($menu[key($menu)]);}
+		}
+	}
+
+	function remove_posts_admin_bar() { 
+		global $wp_admin_bar;
+		$wp_admin_bar->remove_node('new-content');//NOTE-remove entire item, just want it to remove the link
+	}
+	
+	function remove_posts_dashboard() {
+		echo '
+		<style type="text/css">
+		#dashboard_right_now .b-posts,
+		#dashboard_right_now .posts
+		{ display:none !important; }
+		</style>';
+	}
+	
+	
+	//DISABLE ADD NEW PAGE
+	function disable_add_new_page() {
+		add_action('admin_head', array($this, 'hide_add_new_page_button'));
+		add_action('admin_bar_menu', array($this, 'remove_pages_admin_bar'), 1000);
+		add_action('admin_menu', array($this, 'remove_add_page_submenu'));
+	}
+	
+	function remove_add_page_submenu() {
+		global $submenu;
+		unset($submenu['edit.php?post_type=page'][10]);
+	}
+
+	function remove_pages_admin_bar() { 
+		global $wp_admin_bar;
+		$wp_admin_bar->remove_node('new-page');
+	}
+	
 	function hide_add_new_page_button() {
 		global $post;
 		if ($post->post_type == 'page') {
@@ -213,23 +288,17 @@ class OS_CustomAdmin {
 		}
 	}
 	
-	//hide updates nag from non admins
-	function hide_updates_nag() {
-		if (!current_user_can('administrator')) {
-			remove_action('admin_notices', 'update_nag', 3);
-		}
+	//OTHER FUNCTIONS
+	
+	//hide page attributes
+	function hide_page_attributes() {
+		add_action('admin_init', array($this, 'remove_pageparent_div'));
+	}
+	function remove_pageparent_div() {
+		remove_meta_box('pageparentdiv','page','normal');
 	}
 	
-	//custom tinymce editor
-	function custom_tiny_mce($in) {
-		if (!current_user_can('administrator')) {
-			$in['theme_advanced_buttons1']='formatselect,|,bold,italic,|,link,unlink,|,bullist,numlist,|,undo,redo,|,wp_more';//,|,wp_adv';
-			$in['theme_advanced_buttons2']='justifyleft,justifycenter,justifyright,|,forecolor,|,removeformat,|,charmap,|,wp_fullscreen';
-			$in['theme_advanced_buttons3']='';
-			$in['theme_advanced_buttons4']='';
-		}
-		return $in;
-	}
+	
 	
 }
 ?>
